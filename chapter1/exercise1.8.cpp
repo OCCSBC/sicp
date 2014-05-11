f114f292b9d47e886e3a66675f881e010fad790f
Masako Toda <todamasako0421@hotmail.com>
2014-05-11 15:49:21 -0700

    Ooops. Edie is so right! - don't average to improve.

--- chapter1/exercise1.8.cpp
+++ chapter1/exercise1.8.cpp
@@ -3,11 +3,6 @@ float cube(float x)
     return x * x * x;
 }
  
-float average(float x, float y)
-{
-    return (x + y) / 2;
-}
- 
 bool is_good_enough(float guess, float x)
 {
     return abs(cube(guess) - x) < 0.001;
@@ -16,7 +11,7 @@ bool is_good_enough(float guess, float x)
 float improve(float guess, float x)
 {
     float next = ((x / (guess * guess)) + 2 * guess) / 3;
-    return average(guess, next);
+    return next;
 }
  
 float cbrt_itr(float guess, float x)
