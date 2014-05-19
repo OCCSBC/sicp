fe15bd3c501e6c92458f038fe6d7c36ae6078d09
Masako Toda <todamasako0421@hotmail.com>
2014-05-18 22:49:42 -0700

    correction

--- chapter1/exercise1.11.scm
+++ chapter1/exercise1.11.scm
@@ -9,10 +9,28 @@
 
 ; iterative
 (define (f-iter a b c count)
-  (if (< count 3) a
+  (if (< count 1) c
       (f-iter (+ (* 1 a) (* 2 b) (* 3 c)) a b (- count 1))))
 
 (define (f n)
   (f-iter 2 1 0 n))
+
+(f-recursive 0)
+(f 0)
+(f-recursive 1)
+(f 1)
+(f-recursive 2)
+(f 2)
+(f-recursive 3)
+(f 3)
+(f-recursive 4)
+(f 4)
+(f-recursive 10)
+(f 10)
+(f-recursive 11)
+(f 11)
+(f 31)
+
+
 
 
